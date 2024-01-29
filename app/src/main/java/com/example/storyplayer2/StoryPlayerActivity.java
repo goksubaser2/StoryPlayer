118a5fbc8f39189708674f86ceccd838e903e16f
goksubaser <goksubaser@gmail.com>
2024-01-29 10:59:38 +0300

    cleanup code

--- app/src/main/java/com/example/storyplayer2/StoryPlayerActivity.java
+++ app/src/main/java/com/example/storyplayer2/StoryPlayerActivity.java
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
@@ -32,108 +33,13 @@ import java.util.Objects;
 import de.hdodenhof.circleimageview.CircleImageView;
 import jp.shts.android.storiesprogressview.StoriesProgressView;
 
-//TODO
 public class StoryPlayerActivity extends AppCompatActivity {
     private JazzyViewPager vpage;
-//    private int position;
-//    private String[] usernameList;
-//    private String[] ppUrlList;
-//    private final String[] ImageURls = {"https://source.unsplash.com/user/c_v_r/135x240","https://source.unsplash.com/user/c_v_r/150x150"}; //TODO
-//
-//    long pressTime = 0L;
-//    long limit = 500L;
-//
-//    private StoriesProgressView storiesProgressView;
-//    private ImageView image;
-//
-//    private CircleImageView profileImage;
-//    private TextView usernameTV;
-//
-//    private int counter = 0;
-
-//    private final View.OnTouchListener onTouchListener = new View.OnTouchListener() {
-//        @Override
-//        public boolean onTouch(View view, MotionEvent motionEvent) {
-//            switch (motionEvent.getAction()) {
-//                case MotionEvent.ACTION_DOWN:
-//
-//                    // on action down when we press our screen
-//                    // the story will pause for specific time.
-//                    pressTime = System.currentTimeMillis();
-//
-//                    // on below line we are pausing our indicator.
-//                    storiesProgressView.pause();
-//                    return false;
-//                case MotionEvent.ACTION_UP:
-//
-//                    // in action up case when user do not touches
-//                    // screen this method will skip to next image.
-//                    long now = System.currentTimeMillis();
-//
-//                    // on below line we are resuming our progress bar for status.
-//                    storiesProgressView.resume();
-//
-//                    // on below line we are returning if the limit < now - presstime
-//                    return limit < now - pressTime;
-//            }
-//            return false;
-//        }
-//    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_story_player);
-
-//        storiesProgressView = (StoriesProgressView) findViewById(R.id.stories);
-//        storiesProgressView.setStoriesCount(ImageURls.length);
-//        storiesProgressView.setStoryDuration(3000L);
-//        storiesProgressView.setStoriesListener(this);
-//        storiesProgressView.startStories(counter);
-//        image = (ImageView) findViewById(R.id.image);
-
-//        profileImage = findViewById(R.id.profile_image);
-//        usernameTV = findViewById(R.id.usernameTV);
-
-//        this.position = intent.getIntExtra("position",1);
-//        this.usernameList = Objects.requireNonNull(intent.getStringArrayExtra("usernameList"));
-//        this.ppUrlList = Objects.requireNonNull(intent.getStringArrayExtra("ppUrlList"));
-
-//        glideImage(ImageURls[counter]);
-//
-//        // below is the view for going to the previous story.
-//        // initializing our previous view.
-//        View reverse = findViewById(R.id.reverse);
-//
-//        // adding on click listener for our reverse view.
-//        reverse.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // inside on click we are
-//                // reversing our progress view.
-//                storiesProgressView.reverse();
-//            }
-//        });
-//
-//        // on below line we are calling a set on touch
-//        // listener method to move towards previous image.
-//        reverse.setOnTouchListener(onTouchListener);
-//
-//        // on below line we are initializing
-//        // view to skip a specific story.
-//        View skip = findViewById(R.id.skip);
-//        skip.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // inside on click we are
-//                // skipping the story progress view.
-//                storiesProgressView.skip();
-//            }
-//        });
-//        // on below line we are calling a set on touch
-//        // listener method to move to next story.
-//        skip.setOnTouchListener(onTouchListener);
 
         vpage = (JazzyViewPager) findViewById(R.id.jazzy_pager);
         vpage.setTransitionEffect(TransitionEffect.CubeOut);
@@ -157,14 +63,6 @@ public class StoryPlayerActivity extends AppCompatActivity {
         });
     }
 
-//    @Override
-//    protected void onDestroy() {
-//        // in on destroy method we are destroying
-//        // our stories progress view.
-//        storiesProgressView.destroy();
-//        super.onDestroy();
-//    }
-
     private class MainAdapter extends PagerAdapter implements StoriesProgressView.StoriesListener {
         StoriesProgressView storiesProgressView;
         StoryBinding binding;
@@ -176,13 +74,11 @@ public class StoryPlayerActivity extends AppCompatActivity {
                 {"https://source.unsplash.com/user/c_v_r/175x175", "https://source.unsplash.com/user/c_v_r/200x200", "https://source.unsplash.com/user/c_v_r/225x225"},
                 {"https://source.unsplash.com/user/c_v_r/250x250", "https://source.unsplash.com/user/c_v_r/275x275","https://source.unsplash.com/user/c_v_r/300x300", "https://source.unsplash.com/user/c_v_r/325x325"},
                 {"https://source.unsplash.com/user/c_v_r/350x350", "https://source.unsplash.com/user/c_v_r/375x375","https://source.unsplash.com/user/c_v_r/400x400", "https://source.unsplash.com/user/c_v_r/425x425","https://source.unsplash.com/user/c_v_r/450x450"}
-        }; //TODO
+        };
         private int position;
-        private CircleImageView profileImage;
-        private TextView usernameTV;
-        private String[] usernameList;
-        private String[] ppUrlList;
-        private int[] counter;
+        private final String[] usernameList;
+        private final String[] ppUrlList;
+        private final int[] counter;
         long pressTime = 0L;
         long limit = 500L;
 
@@ -192,6 +88,7 @@ public class StoryPlayerActivity extends AppCompatActivity {
             this.ppUrlList = ppUrlList;
             this.counter=counter;
         }
+        @NonNull
         @Override
         public Object instantiateItem(ViewGroup container, final int position) {
             LayoutInflater inflater = (LayoutInflater) container.getContext()
@@ -203,8 +100,8 @@ public class StoryPlayerActivity extends AppCompatActivity {
                 startStories();
             }
 
-            profileImage = binding.profileImage;
-            usernameTV = binding.usernameTV;
+            CircleImageView profileImage = binding.profileImage;
+            TextView usernameTV = binding.usernameTV;
 
             usernameTV.setText(usernameList[position]);
             Glide.with(view)
@@ -217,9 +114,7 @@ public class StoryPlayerActivity extends AppCompatActivity {
             reverse.setOnTouchListener(onTouchListener);
 
             View skip = binding.skip;
-            skip.setOnClickListener(v -> {
-                storiesProgressView.skip();
-            });
+            skip.setOnClickListener(v -> storiesProgressView.skip());
             skip.setOnTouchListener(onTouchListener);
 
             container.addView(view, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
@@ -228,7 +123,7 @@ public class StoryPlayerActivity extends AppCompatActivity {
             return view;
         }
         @Override
-        public void destroyItem(ViewGroup container, int position, Object obj) {
+        public void destroyItem(ViewGroup container, int position, @NonNull Object obj) {
             container.removeView((View) obj);
         }
         @Override
@@ -236,29 +131,25 @@ public class StoryPlayerActivity extends AppCompatActivity {
             return usernameList.length;
         }
         @Override
-        public boolean isViewFromObject(View arg0, Object arg1) {
+        public boolean isViewFromObject(@NonNull View arg0, @NonNull Object arg1) {
             return arg0 == arg1;
         }
 
         @Override
-        public void onNext() {//TODO
-            System.out.println("next");
-            System.out.println(counter[0]+" "+counter[1]+" "+counter[2]+" "+counter[3]+" "+counter[4]);
+        public void onNext() {
             if(ImageURls[position].length>counter[position]) {
                 counter[position]++;
                 glideImage(ImageURls[position][counter[position]]);
             }
         }
         @Override
-        public void onPrev() {//TODO
-            System.out.println("prev");
+        public void onPrev() {
             if ((counter[position] - 1) < 0) {
                 if(position>0) {
                     position--;
                     vpage.setCurrentItem(position);
                     startStories();
                 }
-                System.out.println(position);
                 return;
             }
             --counter[position];
@@ -267,8 +158,7 @@ public class StoryPlayerActivity extends AppCompatActivity {
             }
         }
         @Override
-        public void onComplete() {//TODO
-            System.out.println("complete");
+        public void onComplete() {
             counter[position] = ImageURls[position].length;
             position++;
             vpage.setCurrentItem(position);
@@ -295,7 +185,7 @@ public class StoryPlayerActivity extends AppCompatActivity {
         private void startStories(){
             storiesProgressView = binding.stories;
             storiesProgressView.setStoriesCount(ImageURls[position].length);
-            storiesProgressView.setStoryDuration(8000);
+            storiesProgressView.setStoryDuration(5000);
             storiesProgressView.setStoriesListener(this);
             if(counter[position]<ImageURls[position].length) storiesProgressView.startStories(counter[position]);
             image = binding.image;
